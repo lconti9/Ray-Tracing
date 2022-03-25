f381217886b78fc0dbb36856491c3df02368029a
lucaconti9 <lconti1@crimson.ua.edu>
2022-03-24 22:51:52 -0500

    Added command line status

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main()
 
     for (int j = image_height - 1; j >= 0; --j)
     {
+        std::cerr << "\rScanlines Remaining" << j << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
             auto r = double(i) / (image_width - 1);
