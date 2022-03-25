d3c914698e54b415ad8965f387e9252e5fbc92dd
lucaconti9 <lconti1@crimson.ua.edu>
2022-03-24 23:26:36 -0500

    Added vec3 helper functions

--- vec3.h
+++ vec3.h
@@ -69,3 +69,67 @@ using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
 
 #endif
+
+// vec3 Utility Functions
+
+// Outstream function
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
+{
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+// Vector addition function
+inline vec3 operator+(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+// Vector subtraction function
+inline vec3 operator-(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+// Vector Vector multiplication function
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+// Vector Double multiplication function
+inline vec3 operator*(const vec3 &v, double t)
+{
+    return vec3(v.e[0] * t, v.e[1] * t, v.e[2] * t);
+}
+
+// Double Vector multiplication function
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return v * t;
+}
+
+// Vector double division function
+inline vec3 operator/(const vec3 &v, double t)
+{
+    return (1 / t) * v;
+}
+
+// Vector dot product function
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+// Vector cross product function
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+// Unit vector function
+inline vec3 unit_vector(vec3 &v)
+{
+    return v / v.length();
+}
